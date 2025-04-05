abaff331854507232c1d6e7463c9d37d7a7daef4
nakul-krishnakumar <nakulkrishnakumar86@gmail.com>
2025-04-05 09:12:05 +0000

    Made lex program to input from a file and count comments

---
+++ count_comments/input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+   /* This is a
+      multi-line comment!! */
+   
+      // a single line comment 
+   
+   int x = 10;
+   // a
+   // b ?:"W:>>?"}{<??{}""""} abwhdb // awdwd
+   return 0;
+}
